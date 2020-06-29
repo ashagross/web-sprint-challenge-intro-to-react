13b7f8bdd52bca64cf607fa0db639b46533389b1
Asha Gross <hello@ashagross.com>
2020-06-28 20:23:47 -0400

    MVP Met

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios"
 import './App.css';
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,11 +11,20 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get("https://cors-anywhere.herokuapp.com/swapi.py4e.com/api/people")
+      .then(response => {
+        console.log(response.data.results)
+        setData(response.data.results)
+      })
+  }, [])
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character characterArray={data} />
     </div>
   );
 }
-
 export default App;
